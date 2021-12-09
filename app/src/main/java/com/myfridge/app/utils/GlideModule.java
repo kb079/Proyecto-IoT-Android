ca5070f04ead3d42d310ce578a1dba630d3eb21b
KB <krystian8844@gmail.com>
2021-12-09 23:22:09 +0100

    Programa cámara

--- app/src/main/java/com/myfridge/app/utils/GlideModule.java
+++
@@ -1,6 +0,0 @@
-package com.myfridge.app.utils;
-
-import com.bumptech.glide.module.AppGlideModule;
-
-@com.bumptech.glide.annotation.GlideModule
-public class GlideModule extends AppGlideModule { }
